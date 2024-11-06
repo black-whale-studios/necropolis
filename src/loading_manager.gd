e4a623eb69255b1cdf55c915aac2ae384faeebaf
gu <cockernutx@gmail.com>
2024-11-06 16:11:08 -0300

    Initial commit

---
+++ src/loading_manager.gd
@@ -0,0 +1,48 @@
+extends Node
+
+
+signal progress_changed(progress: Array[float])
+signal loading_done
+
+@onready var _load_screen_scene = preload("res://src/loading_manager/loading_screen.tscn")
+
+var _current_load_screen: Node
+var _loaded_resource: PackedScene
+var _request_scene: String
+var _progress: Array[float]
+var _is_loading: bool
+
+func _process(_delta: float) -> void:
+	if !_is_loading:
+		return
+	
+	var status = ResourceLoader.load_threaded_get_status(_request_scene, _progress)
+	match status:
+		ResourceLoader.THREAD_LOAD_IN_PROGRESS:
+			_current_load_screen.update_progress_bar(_progress)# Change the ProgressBar value
+		ResourceLoader.THREAD_LOAD_LOADED:
+			_is_loading = false
+			_current_load_screen.load_done()
+			_loaded_resource = ResourceLoader.load_threaded_get(_request_scene)
+			get_tree().change_scene_to_packed(_loaded_resource)
+		ResourceLoader.THREAD_LOAD_FAILED:
+			_is_loading = false
+			print("Error. Could not load Resource")
+	pass
+
+func _ready() -> void:
+	_is_loading = false
+	pass
+
+
+
+func load_scene(scene_path: String):
+	_request_scene = scene_path
+	_current_load_screen = _load_screen_scene.instantiate()
+	get_tree().get_root().add_child(_current_load_screen)
+	
+	
+	var state = ResourceLoader.load_threaded_request(_request_scene)
+	if state == OK:
+		_is_loading = true
+	pass
---
+++ src/loading_manager/loading_screen.gd
@@ -0,0 +1,25 @@
+extends Control
+
+
+@onready var progress_bar: ProgressBar = $Panel/ProgressBar
+
+signal loading_screen_ready
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	loading_screen_ready.emit()
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	
+	pass
+
+func update_progress_bar(progress: int):
+	progress_bar.set_value_no_signal(progress * 100)
+	pass
+
+func load_done():
+	self.queue_free()
+	pass
